63c52524d6d45ddf549f230cc3216d69e2bbfd87
Ryan Tan <ryanrongchangtan@gmail.com>
2026-04-17 11:39:12 +0800

    aaa

--- petpal_dashboard/apps/web/.next/server/server-reference-manifest.js
+++ petpal_dashboard/apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9nuPY4xiLggswXucSjAjiGb9pj6Ib6QO/QVlhH4JJk8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
